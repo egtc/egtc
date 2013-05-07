ad2e189e930f52b224301a0ce369d9ea9de0855e
cjones <switchback.junkie@gmail.com>
2013-05-06 23:17:21 -0600

    Add legacy degrees to student show view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,10 @@ class StudentsController < ApplicationController
       search(params[:search], :course_name).
       order_by(parse_sort_param(:term, :course_name))
      # @grades = @grades.course_name_is(params[:course_name]) if params[:course_name] && !params[:course_name].blank?
+       @student = find_instance
+       @banner_degrees = @student.banner_degrees.
+         search(params[:search], :program_code).
+         order_by(parse_sort_param(:term, :graduation_date))
     hobo_show
   end
 
