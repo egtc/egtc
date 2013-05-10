d75953620c920e45ed1c7a0dc39177ec19dd5994
cjones <switchback.junkie@gmail.com>
2013-05-09 19:35:28 -0600

    Add courses to program show view

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -3,5 +3,13 @@ class ProgramsController < ApplicationController
   hobo_model_controller
 
   auto_actions :all
+  
+  def show
+    @program = find_instance
+    @courses = @program.courses.
+      search(params[:search], :name).
+      order_by(parse_sort_param(:name))
+      hobo_show
+    end
 
 end
